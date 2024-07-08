4a30be6db9b049f0ebb666ece055c12fc1d7e322
cotunjr <aytekin.eren55@gmail.com>
2024-07-08 22:02:50 +0000

    just testing

--- device.mk
+++ device.mk
@@ -15,8 +15,8 @@ PRODUCT_PACKAGES += \
     SettingsProviderResMayfly \
     SettingsResMayfly \
     SystemUIResMayfly \
-    WifiResMayfly
-
+    WifiResMayfly \
+    AOSPAMayflyFrameworksOverlay
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.xiaomi
@@ -30,3 +30,39 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/mayfly/mayfly-vendor.mk)
+
+
+# Camera
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.product.mod_device=mayfly
+
+# Characteristics
+PRODUCT_CHARACTERISTICS := nosdcard
+
+
+# Display
+PRODUCT_ODM_PROPERTIES += \
+    vendor.display.disable_3d_adaptive_tm=0 \
+    vendor.display.enable_fb_scaling=1 \
+    vendor.display.enable_rounded_corner=0 \
+    vendor.display.enable_optimize_refresh=1
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.display.ai_disp.enable=true \
+    ro.vendor.display.hwc_thermal_dimming=true \
+    ro.vendor.display.mi_calib.enable=true \
+    ro.vendor.display.nature_mode.enable=true \
+    ro.vendor.histogram.enable=true \
+    ro.vendor.sre.enable=true \
+    ro.vendor.xiaomi.bl.poll=true
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    libudfpshandler
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/init.mayfly.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init.mayfly.rc
+
+# Kernel
+KERNEL_PREBUILT_DIR := $(LOCAL_PATH)-kernel
