58fbe276d07400d7e8c25a3e8ed50e84425dc4fb
cotunjr <75676600+cotunjr@users.noreply.github.com>
2024-07-09 01:27:11 +0300

    cant trust

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,35 +1,47 @@
+# Copyright (C) 2024 Paranoid Android
 #
-# Copyright (C) 2023 The LineageOS Project
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
 #
-# SPDX-License-Identifier: Apache-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 DEVICE_PATH := device/xiaomi/mayfly
 
 # Inherit from sm8450-common
 include device/xiaomi/sm8450-common/BoardConfigCommon.mk
 
-# Properties
-TARGET_SYSTEM_PROP += $(DEVICE_PATH)/properties/system.prop
-
-# Display
-TARGET_SCREEN_DENSITY := 440
-
-# Kernel modules
-BOOT_KERNEL_MODULES += \
-    goodix_fod.ko \
-    qcom_pm8008-regulator.ko \
-    fts_touch_spi.ko \
-    qcom-hv-haptics.ko
-BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD += $(BOOT_KERNEL_MODULES)
-BOARD_VENDOR_KERNEL_MODULES_LOAD += $(BOOT_KERNEL_MODULES)
-
-# Vibrator
-TARGET_QTI_VIBRATOR_EFFECT_LIB := libqtivibratoreffect.xiaomi
-TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM := true
-
-# Recovery
-TARGET_RECOVERY_UI_MARGIN_HEIGHT := 100
-
 # Include proprietary files
 include vendor/xiaomi/mayfly/BoardConfigVendor.mk
+
+# HIDL
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
+
+# OTA
+TARGET_OTA_ASSERT_DEVICE := mayfly|2206123SC
+
+# Partitions
+BOARD_SUPER_PARTITION_SIZE := 9126805504
+BOARD_QTI_DYNAMIC_PARTITIONS_SIZE := 9122611200 # (BOARD_SUPER_PARTITION_SIZE - 4MB overhead)
+
+# Powershare
+TARGET_POWERSHARE_NODE := /sys/class/qcom-battery/reverse_chg_mode
+
+# Sensors
+SOONG_CONFIG_NAMESPACES += SENSORS_XIAOMI
+SOONG_CONFIG_SENSORS_XIAOMI += USES_UDFPS_SENSOR
+SOONG_CONFIG_SENSORS_XIAOMI_USES_UDFPS_SENSOR := true
+
+# Sepolicy
+BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
+
+# Screen density
+TARGET_SCREEN_DENSITY := 440
+
+
