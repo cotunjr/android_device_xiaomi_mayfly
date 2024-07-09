08e5411983361a432a6ca375de2a71f76dfdb1ce
cotunjr <75676600+cotunjr@users.noreply.github.com>
2024-07-09 03:08:15 +0300

    Update extract-files.sh

--- extract-files.sh
+++ extract-files.sh
@@ -1,26 +1,10 @@
 #!/bin/bash
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017-2020 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
-function blob_fixup() {
-    case "${1}" in
-        vendor/etc/camera/mayfly_enhance_motiontuning.xml|vendor/etc/camera/mayfly_motiontuning.xml)
-            sed -i 's/xml=version/xml version/g' "${2}"
-            ;;
-        vendor/etc/camera/pureView_parameter.xml)
-            sed -i 's/=\([0-9]\+\)>/="\1">/g' "${2}"
-            ;;
-        vendor/etc/init/hw/init.batterysecret.rc)
-            sed -i 's/on charger/on property:init.svc.vendor.charger=running/g' "${2}"
-            ;;
-        vendor/lib/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so|vendor/lib64/hw/vendor.xiaomi.sensor.citsensorservice@2.0-impl.so)
-            sed -i 's/_ZN13DisplayConfig10ClientImpl13ClientImplGetENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE/_ZN13DisplayConfig10ClientImpl4InitENSt3__112basic_stringIcNS1_11char_traitsIcEENS1_9allocatorIcEEEEPNS_14ConfigCallbackE\x0\x0\x0\x0\x0\x0\x0\x0\x0\x0/g' "${2}"
-            ;;
-    esac
-}
 
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
@@ -30,9 +14,9 @@ fi
 
 set -e
 
-# Required!
 export DEVICE=mayfly
 export DEVICE_COMMON=sm8450-common
 export VENDOR=xiaomi
+export VENDOR_COMMON=${VENDOR}
 
-"./../../${VENDOR}/${DEVICE_COMMON}/extract-files.sh" "$@"
+"./../../${VENDOR_COMMON}/${DEVICE_COMMON}/extract-files.sh" "$@"
